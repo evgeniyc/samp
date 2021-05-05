5f2f03469aa76b40d8449c0390c69da73e651e26
evgeniyc <evgeniychp@i.ua>
2021-05-05 17:31:26 +0300

    wednesdey work work

--- framework/controllers/BasketController.php
+++ framework/controllers/BasketController.php
@@ -10,37 +10,20 @@ class BasketController extends Controller
 {
     public function actionIndex($id=0,$user=0)
     {
-        if($id && $user) {
+        
 		$session = Yii::$app->session;
-
-		// проверяем что сессия уже открыта
-		//if ($session->isActive) ...
-
-		// открываем сессию
 		$session->open();
-		$session['order.id'] = $id;
-		$session['order.user'] = $user;
-		$session->setFlash('orderCreated', 'Вы успешно добавили заказ');
-		$session->close();	
+		if($id && $user) {
+			$session['order.id'] = $id;
+			$session['order.user'] = $user;
+			$session->setFlash('orderCreated', 'Вы успешно добавили заказ!');
 		}
-		// закрываем сессию
-		//$session->close();
-
-		// уничтожаем сессию и все связанные с ней данные.
-		//$session->destroy();
-		
-		// установка flash-сообщения с названием "postDeleted"
-		//$session->setFlash('postDeleted', 'Вы успешно удалили пост.');
-
-		// Запрос #2
-		// отображение flash-сообщения "postDeleted"
-		//echo $session->getFlash('postDeleted');
-
-		// Запрос #3
-		// переменная $result будет иметь значение false, так как flash-сообщение было автоматически удалено
-		//$result = $session->hasFlash('postDeleted');
-		
-		//return $this->render('index');
+		else {
+			$session->setFlash('orderCreated', 'Для добавления заказа войдите или зарегистрируйтесь!');
+		}
+		$session->close();	
+			
+		return $this->redirect(['products/view','id'=>$id]);
     }
 
 }
